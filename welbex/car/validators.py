de7d19a6e8918c6c33d35a142a05cef889e1ceb0
Ivan Dziov <102470195+ivandziov@users.noreply.github.com>
2023-05-28 18:08:22 +0200

    fix validators.py
    delete print from validator

--- welbex/car/validators.py
+++ welbex/car/validators.py
@@ -11,6 +11,5 @@ def validate_car_number(value: str) -> None:
     if (not value[:4].isdigit()) or (not value[4].isupper()) or (not 'A' <= value[4] <= 'Z'):
         raise ValidationError('The value must contain an English capital letter at the end!')
 
-    print(int(value[:4]))
     if not 1000 <= int(value[:4]) <= 9999:
         raise ValidationError('The number must be in the range from 1000 to 9999 inclusive!')
